9f198db2e89e40e1d10d967da6a08a1adaa05974
Razor <72689902+razorthefolf@users.noreply.github.com.>
2024-07-24 20:14:51 +0200

    Refactored logic and added parent prefixing

--- Editor/PrefixerWindow.cs
+++ Editor/PrefixerWindow.cs
@@ -1,44 +1,28 @@
-using UnityEngine;
 using UnityEditor;
+using UnityEngine;
 
 // ReSharper disable once CheckNamespace
 namespace RT
 {
     public class PrefixerWindow : EditorWindow
     {
-        private GameObject parent;
-        private string prefix;
-        private int selectedSeparator;
-        private bool placeAtStart;
+        private readonly string[] prefixPosition = { "Start", "End" };
 
         private readonly string[] separators =
         {
-            "Dash", "Dot", "Underscore",
+            "Dash", "Dot", "Underscore"
         };
 
-        private readonly string[] prefixPosition = { "Start", "End" };
-
-        private void UpdatePrefixes(Transform parentTransform, string newPrefix, string separator, bool start)
-        {
-            foreach (Transform child in parentTransform)
-            {
-                if (start)
-                {
-                    child.name = newPrefix + separator + child.name;
-                }
-                else
-                {
-                    child.name = child.name + separator + newPrefix;
-                }
-
-                UpdatePrefixes(child, newPrefix, separator, start); // Recursively update prefixes for nested children
-            }
-        }
+        private GameObject parent;
+        private bool placeAtStart = true;
+        private string prefix;
+        private bool prefixParent = true;
+        private int selectedSeparator = 1;
 
 
         private void OnGUI()
         {
-            titleContent = new GUIContent(" Prefixer");
+            titleContent = new GUIContent("Prefixer");
 
             GUILayout.Space(15);
             GUILayout.Label("Prefixer", EditorStyles.boldLabel);
@@ -47,9 +31,10 @@ namespace RT
             prefix = EditorGUILayout.TextField("Prefix", prefix);
             selectedSeparator = EditorGUILayout.Popup("Separator", selectedSeparator, separators);
             placeAtStart = EditorGUILayout.Popup("Position", placeAtStart ? 0 : 1, prefixPosition) == 0;
+            prefixParent = EditorGUILayout.Toggle("Prefix Parent?", prefixParent);
             if (GUILayout.Button("Prefix it!"))
             {
-                if (parent == null)
+                if (!parent)
                 {
                     Debug.LogError("Parent is null");
                     return;
@@ -61,26 +46,38 @@ namespace RT
                     return;
                 }
 
+                string separator = selectedSeparator switch
+                {
+                    0 => "-",
+                    1 => ".",
+                    _ => "_"
+                };
+
                 foreach (Transform child in parent.transform)
                 {
-                    string separator = selectedSeparator switch
-                    {
-                        1 => ".",
-                        2 => "_",
-                        _ => "-"
-                    };
-
-                    if (placeAtStart)
-                    {
-                        child.name = prefix + separator + child.name;
-                    }
-                    else
-                    {
-                        child.name = child.name + separator + prefix;
-                    }
+                    child.name = placeAtStart
+                        ? prefix + separator + child.name
+                        : child.name + separator + prefix;
 
                     UpdatePrefixes(child, prefix, separator, placeAtStart);
                 }
+
+                if (prefixParent)
+                    parent.name = placeAtStart
+                        ? prefix + separator + parent.name
+                        : parent.name + separator + prefix;
+            }
+        }
+
+        private static void UpdatePrefixes(Transform parentTransform, string newPrefix, string separator, bool start)
+        {
+            foreach (Transform child in parentTransform)
+            {
+                child.name = start
+                    ? newPrefix + separator + child.name
+                    : child.name + separator + newPrefix;
+
+                UpdatePrefixes(child, newPrefix, separator, start);
             }
         }
     }
